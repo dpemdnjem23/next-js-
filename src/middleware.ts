74d68c26388b2bffd6e8e2c38a73cc2088947aec
gitkrak <dpemdnjem23@gmail.com>
2024-08-16 15:21:07 +0900

    cartData 분리하기

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,37 +6,40 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useSearchParams } from "next/navigation";
 
-import { supabase } from "./lib";
+import { getUser, supabase } from "./lib";
 
-// const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-// const supabaseServerKey: string = process.env.NEXT_PUBLIC_SUPABASE_ARON_KEY || "";
-
-//middleware로 guest
 export async function middleware(req: NextRequest) {
-  //미들웨어 한개더추가할것 만료된경우 강제 로그아웃 해야함
-
-  // const accessToken = req.headers.get('Authorization')?.replace('Bearer ', '');
-
-  // const { data, error } = await supabase.auth.getSession();
-
-  // console.log(data);
-  // // console.log(session, "session", !session);
-
-  // if (!data) {
-  //세션만료되면 로그아웃
-
-  // const { error } = await supabase.auth.signOut();
-  //로그아웃하면 메인페이지로 이동
-
-  //   return NextResponse.redirect(new URL("/Member/login", req.url));
-  // }
-
-  // return res;
+  console.log("middleware", req.nextUrl.pathname);
+  const session = await getUser();
+  if (!session) {
+    return NextResponse.redirect("httpL//localhost:3000/Member/login");
+  }
 }
 
-// export const config = {
-//   matcher: ["/Member/mypage", "/order/:order_code*"],
-// };
+export const config = {
+  matcher: ["/Member/mypage", "/order/:order_code*"],
+};
+
+//미들웨어 한개더추가할것 만료된경우 강제 로그아웃 해야함
+
+// const accessToken = req.headers.get('Authorization')?.replace('Bearer ', '');
+
+// const { data, error } = await supabase.auth.getSession();
+
+// console.log(data);
+// // console.log(session, "session", !session);
+
+// if (!data) {
+//세션만료되면 로그아웃
+
+// const { error } = await supabase.auth.signOut();
+//로그아웃하면 메인페이지로 이동
+
+//   return NextResponse.redirect(new URL("/Member/login", req.url));
+// }
+
+// return res;
+// }
 
 // // /, "/Order/:order_code*"
 
